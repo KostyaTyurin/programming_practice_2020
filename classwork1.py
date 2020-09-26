0e78ea7dee535ecf1e071034e20c488f29c8ca37
KostyaTyurin <tiurin.ka@phystech.edu>
2020-09-26 21:35:43 +0300

    конечный вариант

--- classwork1.py
+++ classwork1.py
@@ -1,5 +1,5 @@
 import numpy as np
-x = list(map(int, input().split()))
+x = list(map(int, input('Введите числа ').split()))
 n = len(x)
 for i in range(n):
     y = (np.log(1/np.e**np.sin(x[i])+1)-(np.log((5/4) + 1/x[i]**15 )))/np.log(1+x[i]**2)
