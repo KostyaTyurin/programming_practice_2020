36e4e62454cadd8e1520301a475aab7ea5fba7bc
KostyaTyurin <tiurin.ka@phystech.edu>
2020-09-26 10:39:54 +0300

    add exp except e

--- classwork1.py
+++ classwork1.py
@@ -2,5 +2,5 @@ import numpy as np
 x = list(map(int, input().split()))
 n = len(x)
 for i in range(n):
-    y = (np.log(1/np.e**np.sin(x[i])+1)-(np.log((5/4) + 1/x[i]**15 )))/np.log(1+x[i]**2)
+    y = (np.log(1/np.exp**np.sin(x[i])+1)-(np.log((5/4) + 1/x[i]**15 )))/np.log(1+x[i]**2)
     print('y(',x[i],')=',y)
---
+++ classwork3.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x= np.arange(-10, 10.01, 0.01)
+plt.plot(x,)
+y=np.log(x**2 + 1) + np.log(np.exp**(-abs(x)/10))
