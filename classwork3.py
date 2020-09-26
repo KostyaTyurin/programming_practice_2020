5ee02e855f938f138e92194576ab25a7473daade
KostyaTyurin <tiurin.ka@phystech.edu>
2020-09-26 11:03:14 +0300

    add classwork3.py

--- classwork3.py
+++ classwork3.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(-1000, 1000.01, 0.01)
+x = np.arange(-100, 100.01, 0.01)
 plt.plot(x, (np.log(x**2 + 1) + np.log(np.exp(abs(x)/(-10)))/np.log(1 + np.tan(1/(1+np.sin(x)**2)))))
 plt.grid(color='red', linewidth=0.5)
 plt.show()
