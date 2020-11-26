56c668a95867cea3ecf1314d4a789d46ab07eaa3
kostya <tiurin.ka@phystech.edu>
2020-11-27 00:01:43 +0300

    add hometask (OOP classes) into week 9

--- week8/5.py
+++ week8/5.py
@@ -2,16 +2,8 @@ import numpy as np
 A = np.array([[1, np.nan, 3],[np.nan, np.nan, np.nan],[np.nan, 2, 3], [4, 5, np.nan]])
 
 
+print(np.where(np.isnan(A), np.nanmean(A[np.argwhere(np.isnan(A))[0]]), A))
 
-def ma(A):
-   shape = np.array(np.shape(A))
-   for i in range(shape[0]):
-       if len(A[i][np.isfinite(A[i])]) == 0:
-            A[i] = np.nan_to_num(A[i])
-       else:
-           A[i][np.isnan(A[i])] = np.nanmean(A[i])
-   return print(A)
-ma(A)
 
 
 
